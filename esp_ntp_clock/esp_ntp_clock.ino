082c5acc84d4cb5856e43eaba9a8a5e81839a813
Kkmonster <kkmonster@hotmail.com>
2015-08-09 17:34:49 +0700

    version 1

--- esp_ntp_clock/esp_ntp_clock.ino
+++ esp_ntp_clock/esp_ntp_clock.ino
@@ -1,21 +1,39 @@
 #define time_zone 3600*7
 
+//Pin connected to ST_CP of 74HC595
+#define latchPin   4
+//Pin connected to SH_CP of 74HC595
+#define ClK_spi  14
+////Pin connected to DS of 74HC595
+#define DS   13
+////Pin connected to dot led
+#define dot   12
+
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <WiFiHelper.h>
+#include <Ticker.h>
 
-// OLED  
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <ESP_Adafruit_SSD1306.h>
+int LED_SEG_TAB[] = {0xfc, 0xc0, 0x6e, 0xe6, 0xd2, 0xb6, 0xbe, 0xe0, 0xfe, 0xf6, 0x00};
+byte dot_state = 0;
+unsigned long epoch ;
+int hh;
+int mm;
+int ss;
+int force_update= 0;
+Ticker second_tick;
+    // // OLED  
+    // #include <Wire.h>
+    // #include <Adafruit_GFX.h>
+    // #include <ESP_Adafruit_SSD1306.h>
 
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-//
+    // #define OLED_RESET 4
+    // Adafruit_SSD1306 display(OLED_RESET);
+    // #define NUMFLAKES 10
+    // #define XPOS 0
+    // #define YPOS 1
+    // #define DELTAY 2
+    // //
 
 const char* ssid     = "MAKERCLUB-CM";
 const char* pass     = "welcomegogogo";
@@ -39,24 +57,33 @@ void init_wifi()
 {
     wifi.on_connecting([](const void* message)
     {
-        Serial.println ("connecting..");
+         Serial.println ("connecting..");
     });
 
     wifi.on_connected([](const void* message)
     {
-        Serial.println ((char*)message);
-        Serial.println("IP address: ");
-        Serial.println(WiFi.localIP());     
+        // Serial.println ((char*)message);
+        // Serial.println("IP address: ");
+        // Serial.println(WiFi.localIP());     
 
-        //
+        // //
         Serial.println("Starting UDP");
+
         udp.begin(localPort);
-        Serial.print("Local port: ");
-        Serial.println(udp.localPort());
+
+        // Serial.print("Local port: ");
+        // Serial.println(udp.localPort());
         //   
     });
 
     wifi.begin();
+    // while (WiFi.status() != WL_CONNECTED) 
+    // {
+    //     delay(100);
+    // }
+    // udp.begin(localPort);
+
+
 }
 
 void init_hardware()
@@ -65,27 +92,72 @@ void init_hardware()
     delay(10);
     Serial.println();
     Serial.println();
+    //set pins to output so you can control the shift register
+    pinMode(latchPin, OUTPUT);
+    pinMode(ClK_spi, OUTPUT);
+    pinMode(DS, OUTPUT);
+    pinMode(dot, OUTPUT);
 
-
-    // OLED 
-
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-    delay(100);
-    display.clearDisplay();
-    //
+    
 }
 
 void setup()
 {
     init_hardware();
     init_wifi();
+    // while(force_update != 1)
+    {
+        NTP_get();
+        delay(100);
+    }
+
+    hh = (epoch % 86400L) / 3600;
+    mm = (epoch % 3600) / 60;
+    ss = (epoch % 60);
+
+    second_tick.attach(1, tick);
 }
 
 void loop()
 {
     wifi.loop();
     NTP_get();
+    delay(1000);
+    if (force_update == 0)
+    {
+        delay(28000);
+    }
+    
 }
+
+void tick (void)
+{
+    epoch++; //Add a second
+    dot_state = 1 - dot_state;
+    digitalWrite(dot, dot_state);
+
+    hh = (epoch % 86400L) / 3600;
+    mm = (epoch % 3600) / 60;
+    ss = (epoch % 60);
+
+int hh1 = hh / 10;
+int hh0 = hh % 10;
+
+if (hh1 == 0) hh1 = 11;
+
+int mm1 = mm / 10;
+int mm0 = mm % 10;
+
+    digitalWrite(latchPin, LOW);
+    shiftOut(DS, ClK_spi, LSBFIRST, LED_SEG_TAB[mm0]);  // digi 0
+    shiftOut(DS, ClK_spi, LSBFIRST, LED_SEG_TAB[mm1]);  // digi 1
+    shiftOut(DS, ClK_spi, LSBFIRST, LED_SEG_TAB[hh0]);  // digi 2
+    shiftOut(DS, ClK_spi, LSBFIRST, LED_SEG_TAB[hh1]);  // digi 3
+    digitalWrite(latchPin, HIGH);
+
+
+}
+
 
 void NTP_get(void)
 {
@@ -95,11 +167,12 @@ void NTP_get(void)
   
   int cb = udp.parsePacket();
   if (!cb) {
-    Serial.println("no packet yet");
+    force_update = 1;
   }
   else {
-    Serial.print("packet received, length=");
-    Serial.println(cb);
+    force_update = 0;
+    // Serial.print("packet received, length=");
+    // Serial.println(cb);
     // We've received a packet, read the data from it
     udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
 
@@ -111,77 +184,45 @@ void NTP_get(void)
     // combine the four bytes (two words) into a long integer
     // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
+    // Serial.print("Seconds since Jan 1 1900 = " );
+    // Serial.println(secsSince1900);
 
     // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
+    // Serial.print("Unix time = ");
     // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
     const unsigned long seventyYears = 2208988800UL;
     // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
+    epoch = secsSince1900 - seventyYears + time_zone;
+    // // print Unix time:
+    // Serial.println(epoch);
 
+    // // print the hour, minute and second:
+    // Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
 
-    // printer to OLED 
-        display.clearDisplay();
-        display.setTextSize(2);
-        display.setTextColor(WHITE);
-        display.setCursor(10,10);
-        display.clearDisplay();
-    //
+    // Serial.print(((epoch + time_zone)  % 86400L) / 3600); // print the hour (86400 equals secs per day)
 
+    // Serial.print(':');
 
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    //
-        display.print(" Time is");
-    //
-    Serial.print(((epoch + time_zone)  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    //
-        display.setCursor(20,35);
-        display.print(((epoch + time_zone)  % 86400L) / 3600);
-    //
-    Serial.print(':');
-    //
-        display.print(":");
-    //
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-      //
-            display.print("0");
-      //
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    //
-        display.print((epoch  % 3600) / 60);
-    //
-    Serial.print(':');
-    //
-        display.print(":");
-    //
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-      //
-            display.print("0");
-      //
-    }
-    Serial.println(epoch % 60); // print the second
-    //
-        display.print(epoch % 60);
-    //
+    // if ( ((epoch % 3600) / 60) < 10 ) {
+    //   // In the first 10 minutes of each hour, we'll want a leading '0'
+    //   Serial.print('0');
+
+    // }
+    // Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+
+    // Serial.print(':');
+
+    // if ( (epoch % 60) < 10 ) {
+    //   // In the first 10 seconds of each minute, we'll want a leading '0'
+    //   Serial.print('0');
+
+    // }
+    // Serial.println(epoch % 60); // print the second
+
   }
   // wait ten seconds before asking for the time again
-
-  //
-      display.display();
-  //
-
-  delay(100);
 }
+
 
 // send an NTP request to the time server at the given address
 unsigned long sendNTPpacket(IPAddress& address)
